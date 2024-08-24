ea8c382261c971c6dc1b54c5361bd57866e832ba
sunrisehouse <ajtwlswjddnv1102@gmail.com>
2024-08-25 03:06:56 +0900

    feat: parameters

--- src/Acquisition.js
+++ src/Acquisition.js
@@ -8,31 +8,170 @@ import { Download, PauseCircle, Pending, PlayCircleFilled, RestartAlt, StopCircl
 import CircularBuffer from './CircularBuffer';
 import { useSearchParams } from 'react-router-dom';
 
-
 const decibelBuffer = new CircularBuffer(10000);
 const accelerometerBuffer = new CircularBuffer(1000);
 const gyroscopeBuffer = new CircularBuffer(1000);
 
-const addDecibelBuffer = (newData) => {
-  decibelBuffer.push(newData);
-};
+let audioContext = null;
+const initAudio = async () => {
+  try {
+    // 오디오 컨텍스트 생성
+    audioContext = new (window.AudioContext || window.webkitAudioContext)();
+  
+    // 오디오 워크렛 모듈을 추가 (my-processor.js)
+    await audioContext.audioWorklet.addModule('/maraca/build/audio-processor.js');
+  
+    // AudioWorkletNode 생성
+    const audioNode = new AudioWorkletNode(audioContext, 'audio-processor');
+  
+    // 사용자로부터 마이크 접근 권한 요청
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+  
+    // 마이크 스트림 생성
+    const microphone = audioContext.createMediaStreamSource(stream);
+  
+    // 마이크 -> AudioWorkletNode -> 출력
+    microphone.connect(audioNode);
+    audioNode.connect(audioContext.destination);
+  
+    audioNode.port.onmessage = (event) => {
+      const { decibel } = event.data;
+      const newMetric = {
+        t: Date.now(),
+        d: decibel,
+      }
+      decibelBuffer.push(newMetric);
+    };
+  } catch (error) {
+    console.error(error);
+  }
+}
+initAudio();
 
-const addAccelerometerBuffer = (newData) => {
-  accelerometerBuffer.push(newData);
-};
+let accelerometer = null;
+const initAccelerometer = async () => {
+  const accPermissionResult = await navigator.permissions.query({ name: "accelerometer" });
+  
+  if (accPermissionResult.state === "denied") {
+    alert("Permission to use accelerometer. gyroscope sensor is denied");
+    return () => {};
+  }
+  
+  if (!'Accelerometer' in window) {
+    alert('브라우저가 센서를 지원하지 않습니다.');
+    return () => {};
+  }
 
-const addGyroscopeBuffer = (newData) => {
-  gyroscopeBuffer.push(newData);
-};
+  try {
+    accelerometer = new window.Accelerometer({ frequency: 10 });
+    accelerometer.addEventListener("reading", () => {
+      const now = Date.now()
+      const newMetric = {
+        t: now,
+        x: accelerometer.x,
+        y: accelerometer.y,
+        z: accelerometer.z,
+        a: Math.sqrt(accelerometer.x ** 2 + accelerometer.y ** 2 + accelerometer.z ** 2),
+      }
+      accelerometerBuffer.push(newMetric);
+    });
+  } catch (error) {
+    if (error.name === 'SecurityError') {
+      alert('Sensor construction was blocked by the Permissions Policy.');
+    } else if (error.name === 'ReferenceError') {
+      alert('Sensor is not supported by the User Agent.');
+    } else {
+      alert(`${error.name} ${error.message}`);
+    }
+  }
+}
+initAccelerometer();
 
+let gyroscope = null;
+const initGyroscope = async () => {
+  const gyroPermissionResult = await navigator.permissions.query({ name: "gyroscope" });
+  
+  if (gyroPermissionResult.state === "denied") {
+    alert("Permission to use accelerometer. gyroscope sensor is denied");
+    return () => {};
+  }
+
+  if (!'Gyroscope' in window) {
+    alert('브라우저가 센서를 지원하지 않습니다.');
+    return () => {};
+  }
+
+  try {
+    gyroscope = new window.Gyroscope({ frequency: 10 });
+    gyroscope.addEventListener("reading", () => {
+      const now = Date.now()
+      const newMetric = {
+        t: now,
+        x: gyroscope.x,
+        y: gyroscope.y,
+        z: gyroscope.z,
+        a: Math.sqrt(gyroscope.x ** 2 + gyroscope.y ** 2 + gyroscope.z ** 2),
+      }
+      gyroscopeBuffer.push(newMetric);
+    });
+  } catch (error) {
+    if (error.name === 'SecurityError') {
+      alert('Sensor construction was blocked by the Permissions Policy.');
+    } else if (error.name === 'ReferenceError') {
+      alert('Sensor is not supported by the User Agent.');
+    } else {
+      alert(`${error.name} ${error.message}`);
+    }
+  }
+};
+initGyroscope();
+
+const saveExcelFile = (decibelMetrics, accelerometerMetrics, gyroscopeMetrics) => {
+  // Decibel 데이터를 배열 형식으로 변환
+  const decibelData = decibelMetrics.map(metric => ({
+    Time: new Date(metric.t).toLocaleString(),
+    Decibel: metric.d,
+  }));
+
+  // Accelerometer 데이터를 배열 형식으로 변환
+  const accelData = accelerometerMetrics.map(metric => ({
+    Time: new Date(metric.t).toLocaleString(),
+    Ax: metric.x,
+    Ay: metric.y,
+    Az: metric.z,
+    "Linear Acceleration": metric.a,
+  }));
+
+  const gyroData = gyroscopeMetrics.map(metric => ({
+    Time: new Date(metric.t).toLocaleString(),
+    Rx: metric.x,
+    Ry: metric.y,
+    Rz: metric.z,
+    "Angular Acceleration": metric.a,
+  }));
+
+  // 두 개의 워크시트로 데이터를 추가
+  const wb = XLSX.utils.book_new();
+  const wsDecibel = XLSX.utils.json_to_sheet(decibelData);
+  const wsAccel = XLSX.utils.json_to_sheet(accelData);
+  const wsGyro = XLSX.utils.json_to_sheet(gyroData);
+
+  XLSX.utils.book_append_sheet(wb, wsDecibel, "Decibel Data");
+  XLSX.utils.book_append_sheet(wb, wsAccel, "Accelerometer Data");
+  XLSX.utils.book_append_sheet(wb, wsGyro, "Gyroscope Data");
+
+  // Excel 파일로 변환
+  const wbout = XLSX.write(wb, { bookType: "xlsx", type: "array" });
+
+  // 파일 저장
+  const blob = new Blob([wbout], { type: "application/octet-stream" });
+  saveAs(blob, "Time_Date.xlsx");
+};
 
 function Acquisition() {
-  const [audioContext, setAudioContext] = useState(null);
-  const [audioWorkletNode, setAudioWorkletNode] = useState(null);
-  const [accelerometer, setAccelerometer] = useState(null);
-  const [gyroscope, setGyroscope] = useState(null);
   const [isMeasuring, setIsMeasuring] = useState(false);
   const [intervalId, setIntervalId] = useState(null);
+  const [maxRunTimeoutId, setMaxRunTimeoutId] = useState(null);
 
   const [decibelMetrics, setDecibelMetrics] = useState([]);
   const [accelerometerMetrics, setAccelerometerMetrics] = useState([]);
@@ -43,168 +182,21 @@ function Acquisition() {
   const tWaiting = Number(searchParams.get('tWaiting'));
   const tMaxRun = Number(searchParams.get('tMaxRun'));
 
+  const [restartTime, setRestartTime] = useState(0);
+  const [remainingTime, setRemainingTime] = useState(tMaxRun);
   const [isTWatingReached, setIsTWatingReached] = useState(false);
-  const [isTMaxRunReached, setTMaxRunReached] = useState(false);
-
-  useEffect(() => {
-    let accel = null;
-    const init = async () => {
-      const accPermissionResult = await navigator.permissions.query({ name: "accelerometer" });
-      
-      if (accPermissionResult.state === "denied") {
-        alert("Permission to use accelerometer. gyroscope sensor is denied");
-        return () => {};
-      }
-      
-      if (!'Accelerometer' in window) {
-        alert('브라우저가 센서를 지원하지 않습니다.');
-        return () => {};
-      }
-
-      try {
-        accel = new window.Accelerometer({ frequency: 10 });
-        accel.addEventListener("reading", () => {
-          const now = Date.now()
-          const newMetric = {
-            t: now,
-            x: accel.x,
-            y: accel.y,
-            z: accel.z,
-            a: Math.sqrt(accel.x ** 2 + accel.y ** 2 + accel.z ** 2),
-          }
-          addAccelerometerBuffer(newMetric);
-        });
-        setAccelerometer(accel);
-      } catch (error) {
-        if (error.name === 'SecurityError') {
-          alert('Sensor construction was blocked by the Permissions Policy.');
-        } else if (error.name === 'ReferenceError') {
-          alert('Sensor is not supported by the User Agent.');
-        } else {
-          alert(`${error.name} ${error.message}`);
-        }
-      }
-    }
-    
-    init();
-
-    return () => {
-      if (accel) {
-        accel.stop();
-      }
-    };
-  }, []);
-
-  useEffect(() => {
-    let gyro = null;
-    const init = async () => {
-      const gyroPermissionResult = await navigator.permissions.query({ name: "gyroscope" });
-      
-      if (gyroPermissionResult.state === "denied") {
-        alert("Permission to use accelerometer. gyroscope sensor is denied");
-        return () => {};
-      }
-
-      if (!'Gyroscope' in window) {
-        alert('브라우저가 센서를 지원하지 않습니다.');
-        return () => {};
-      }
-  
-      try {
-        gyro = new window.Gyroscope({ frequency: 10 });
-        gyro.addEventListener("reading", () => {
-          const now = Date.now()
-          const newMetric = {
-            t: now,
-            x: gyro.x,
-            y: gyro.y,
-            z: gyro.z,
-            a: Math.sqrt(gyro.x ** 2 + gyro.y ** 2 + gyro.z ** 2),
-          }
-          addGyroscopeBuffer(newMetric);
-        });
-        setGyroscope(gyro);
-      } catch (error) {
-        if (error.name === 'SecurityError') {
-          alert('Sensor construction was blocked by the Permissions Policy.');
-        } else if (error.name === 'ReferenceError') {
-          alert('Sensor is not supported by the User Agent.');
-        } else {
-          alert(`${error.name} ${error.message}`);
-        }
-      }
-    };
-
-    init();
-
-    return () => {
-      if (gyro) {
-        gyro.stop();
-      }
-    };
-  }, []);
-
-  const initAudio = useCallback(async () => {
-    try {
-      // 오디오 컨텍스트 생성
-      const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-
-      // 오디오 워크렛 모듈을 추가 (my-processor.js)
-      await audioCtx.audioWorklet.addModule('/maraca/build/audio-processor.js');
-
-      // AudioWorkletNode 생성
-      const audioNode = new AudioWorkletNode(audioCtx, 'audio-processor');
-
-      // 사용자로부터 마이크 접근 권한 요청
-      const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
-
-      // 마이크 스트림 생성
-      const microphone = audioCtx.createMediaStreamSource(stream);
-
-      // 마이크 -> AudioWorkletNode -> 출력
-      microphone.connect(audioNode);
-      audioNode.connect(audioCtx.destination);
-
-      audioNode.port.onmessage = (event) => {
-        const { decibel } = event.data;
-        const newMetric = {
-          t: Date.now(),
-          d: decibel,
-        }
-        addDecibelBuffer(newMetric);
-      };
-
-      setAudioContext(audioCtx);
-      setAudioWorkletNode(audioNode);
-    } catch (error) {
-      console.error(error);
-    }
-  }, []);
-
-  const destroyAudio = useCallback(() => {
-    if (audioContext)
-    if (audioContext && audioContext.state !== 'suspended') audioContext.close();
-    if (audioContext && audioContext.state === 'running') audioContext.suspend();
-
-    navigator.mediaDevices
-      .getUserMedia({ audio: true })
-      .then((stream) => {
-        stream.getTracks().forEach(track => track.stop()); // 마이크 트랙을 중지
-      }).catch(error => console.error("Error stopping media stream:", error));
-    setAudioContext(null);
-    setAudioWorkletNode(null);
-  }, [audioContext]);
+  const [isTMaxRunReached, setIsTMaxRunReached] = useState(false);
 
   useEffect(() => {
     return () => {
-      if (audioContext) {
-        audioContext.close();
-      }
+      if (accelerometer) accelerometer.stop();
+      if (gyroscope) gyroscope.stop();
+      if (audioContext && audioContext.state !== "closed") audioContext.close();
     };
-  }, [audioContext]);
+  }, [accelerometer, gyroscope, audioContext]);
 
-  const initInterval = () => {
-    const newIntervalId = setInterval(() => {
+  const setFetchMetricsInterval = () => {
+    setIntervalId(setInterval(() => {
       const decibelData = decibelBuffer.getLast();
       if (decibelData) {
         setDecibelMetrics((prevData) => [
@@ -238,31 +230,35 @@ function Acquisition() {
           },
         ]);
       }
-    }, tInterval);
-    
-    setIntervalId(newIntervalId);
+    }, tInterval));
   };
 
-  const destroyInterval = useCallback(() => {
-    if (intervalId) clearInterval(intervalId);
-    setIntervalId(null);
-  }, [intervalId]);
+  const setMaxRunTimeout = (time) => {
+    setMaxRunTimeoutId(setTimeout(() => {
+      setIsTMaxRunReached(true);
+      stopMeasurement();
+    }, time));
+  };
 
   const startMeasurement = () => {
-    initAudio();
-    if (accelerometer) accelerometer.start();
-    if (gyroscope) gyroscope.start();
-    initInterval();
+    setFetchMetricsInterval();
+    setRestartTime(Date.now());
+    setMaxRunTimeout(remainingTime);
     setIsMeasuring(true);
   };
 
   const stopMeasurement = useCallback(() => {
-    destroyAudio();
-    if (accelerometer) accelerometer.stop();
-    if (gyroscope) gyroscope.stop();
-    destroyInterval();
+    if (intervalId) {
+      clearInterval(intervalId);
+      setIntervalId(null);
+    }
+    if (maxRunTimeoutId) {
+      clearTimeout(maxRunTimeoutId);
+      setMaxRunTimeout(null);
+    }
+    setRemainingTime(prev => prev - (Date.now() - restartTime));
     setIsMeasuring(false);
-  }, [destroyAudio, accelerometer, gyroscope, destroyInterval]);
+  }, [intervalId, maxRunTimeoutId]);
 
   const handleReset = useCallback(() => {
     setAccelerometerMetrics([]);
@@ -275,53 +271,11 @@ function Acquisition() {
       setIsTWatingReached(true);
       if (!isMeasuring) startMeasurement();
     }, tWaiting);
-    setTimeout(() => {
-      setTMaxRunReached(true);
-      if (isMeasuring) stopMeasurement();
-    }, tMaxRun);
   }, []);
 
   const handleSave = () => {
-    // Decibel 데이터를 배열 형식으로 변환
-    const decibelData = decibelMetrics.map(metric => ({
-      Time: new Date(metric.t).toLocaleString(),
-      Decibel: metric.d,
-    }));
-    
-    // Accelerometer 데이터를 배열 형식으로 변환
-    const accelData = accelerometerMetrics.map(metric => ({
-      Time: new Date(metric.t).toLocaleString(),
-      Ax: metric.x,
-      Ay: metric.y,
-      Az: metric.z,
-      "Linear Acceleration": metric.a,
-    }));
-
-    const gyroData = gyroscopeMetrics.map(metric => ({
-      Time: new Date(metric.t).toLocaleString(),
-      Rx: metric.x,
-      Ry: metric.y,
-      Rz: metric.z,
-      "Angular Acceleration": metric.a,
-    }));
-
-    // 두 개의 워크시트로 데이터를 추가
-    const wb = XLSX.utils.book_new();
-    const wsDecibel = XLSX.utils.json_to_sheet(decibelData);
-    const wsAccel = XLSX.utils.json_to_sheet(accelData);
-    const wsGyro = XLSX.utils.json_to_sheet(gyroData);
-
-    XLSX.utils.book_append_sheet(wb, wsDecibel, "Decibel Data");
-    XLSX.utils.book_append_sheet(wb, wsAccel, "Accelerometer Data");
-    XLSX.utils.book_append_sheet(wb, wsGyro, "Gyroscope Data");
-
-    // Excel 파일로 변환
-    const wbout = XLSX.write(wb, { bookType: "xlsx", type: "array" });
-
-    // 파일 저장
-    const blob = new Blob([wbout], { type: "application/octet-stream" });
-    saveAs(blob, "Time_Date.xlsx");
-  }
+    saveExcelFile(decibelMetrics, accelerometerMetrics, gyroscopeMetrics);
+  };
 
   const decibelMetric = decibelMetrics.length > 0
     ? decibelMetrics[decibelMetrics.length - 1]
@@ -360,18 +314,18 @@ function Acquisition() {
             >
               {!isTWatingReached ? "wating" : isMeasuring ? "pause " : "restart"}
             </Button>
-            <Button
+            {/* <Button
               variant="contained"
               onClick={handleReset}
               disabled={!isTWatingReached || isTMaxRunReached || isMeasuring}
               startIcon={<RestartAlt />}
             >
               reset
-            </Button>
+            </Button> */}
             <Button
               variant="contained"
               onClick={handleSave}
-              disabled={!isTWatingReached || isTMaxRunReached || isMeasuring}
+              disabled={!isTWatingReached || isMeasuring}
               startIcon={<Download />}
             >
               save
